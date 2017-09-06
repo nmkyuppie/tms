a0f4141f81c5b2728816b12347ab6bd98690a32c
nmkyuppie <nmkyuppie@gmail.com>
2017-09-06 09:48:05 +0530

    Display trip details completed
    Change-Id: I1783b0fb0e0b9ae5c3b9f553ebfc78bfd6975481

--- js/tripdetails.js
+++ js/tripdetails.js
@@ -2,12 +2,9 @@
  * 
  */
 
-$( document ).ready(function() {
-	$(function () {
-		  $('#datetimepicker1').datetimepicker();
-		});
-
-});
+var init = function() {
+	getTripDetails();
+}
 
 $(this.window).on('hashchange', function() {
 	var hash=window.location.hash;
@@ -20,6 +17,68 @@ var openAddModal = function (){
 	$('#addModal').modal('show');
 }
 
+var getTripDetails = function(){
+	var tripDetails = Parse.Object.extend("tripdetails");
+	var query = new Parse.Query(tripDetails);
+
+	query.find({
+		success: function(data) {
+			var json=JSON.parse(JSON.stringify(data));
+			drawTripDetails(json);//[0].objectId);
+		},
+		error: function(tripDetails, error) {
+			// The object was not refreshed successfully.
+			// error is a Parse.Error with an error code and message.
+		}
+	});
+}
+
+var drawTripDetails = function(tripDetails){
+	var htmlContent="";
+	for (var i = 0; i < tripDetails.length; i++) {
+		htmlContent+="<tr>";
+		htmlContent+="<td>";
+		htmlContent+="<button type=\"button\" class=\"btn btn-sm btn-primary float-left\" style=\"margin-right: 10px;\">"+
+					 "<i class=\"material-icons md-18\">mode_edit</i>"+
+					 "</button>";
+		htmlContent+="</td>";
+		htmlContent+="<td>";
+		htmlContent+="<button type=\"button\" class=\"btn btn-sm btn-danger float-left\">"+
+					 "<i class=\"material-icons md-18\">delete</i>"+
+					 "</button>";
+		htmlContent+="</td>";
+		htmlContent+="<td>"+
+						$.datepicker.formatDate('dd M yy', new Date(tripDetails[i].tripdate.iso))
+		htmlContent+="</td>";
+		htmlContent+="</td>";
+		htmlContent+="<td>"+
+		tripDetails[i].route;
+htmlContent+="</td>";
+htmlContent+="<td>"+
+tripDetails[i].startingkm;
+htmlContent+="</td>";
+htmlContent+="<td>"+
+tripDetails[i].closingkm;
+htmlContent+="</td>";
+htmlContent+="<td>"+
+tripDetails[i].totalkm;
+htmlContent+="</td>";
+htmlContent+="<td>"+
+tripDetails[i].passing;
+htmlContent+="</td>";
+htmlContent+="<td>"+
+tripDetails[i].toll;
+htmlContent+="</td>";
+htmlContent+="<td>"+
+tripDetails[i].amtperkm;
+htmlContent+="</td>";
+htmlContent+="<td>"+
+tripDetails[i].totalamt;
+htmlContent+="</td>";
+	}
+	$('#tripDetailsTableBody').html(htmlContent);
+}
+
 var save = function(){
 	var tripdate = new Date($('#datepicker').val());
 	var route = $('#src_route').val()+$('#desc_route').val();
@@ -30,7 +89,7 @@ var save = function(){
 	var tollCount = $('#tollcount').val();
 	var amtPerKM = parseInt($('#amtperkm').val());
 	var totalAmount = parseInt($('#totalamt').val());
-	
+
 	var TripDetails = Parse.Object.extend("tripdetails");
 	var tripDetails = new TripDetails();
 
@@ -45,14 +104,14 @@ var save = function(){
 	tripDetails.set("totalamt", totalAmount);
 
 	tripDetails.save(null, {
-	  success: function(tripDetails) {
-	    // Execute any logic that should take place after the object is saved.
-	    alert('New object created with objectId: ' + tripDetails.id);
-	  },
-	  error: function(tripDetails, error) {
-	    // Execute any logic that should take place if the save fails.
-	    // error is a Parse.Error with an error code and message.
-	    alert('Failed to create new object, with error code: ' + error.message);
-	  }
+		success: function(tripDetails) {
+			// Execute any logic that should take place after the object is saved.
+			alert('New object created with objectId: ' + tripDetails.id);
+		},
+		error: function(tripDetails, error) {
+			// Execute any logic that should take place if the save fails.
+			// error is a Parse.Error with an error code and message.
+			alert('Failed to create new object, with error code: ' + error.message);
+		}
 	});
 }
